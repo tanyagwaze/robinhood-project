858907d5d1dabd00d1ac90847115d19cc5cd6480
Tanyaradzwa Gwaze <tanyaradzwagwaze@gmail.com>
2025-07-28 16:18:39 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*Header*/}
+      <div className="app-header">
+      <Header />
+      </div>
+      {/*Body*/}
+      <div className="app-body">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
